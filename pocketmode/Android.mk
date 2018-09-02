3db2c75ee1905af517f739b84cb30d834d1f90db
liquid0624 <rjbirnesser@gmail.com>
2018-09-02 17:39:29 -0400

    sdm845-common: import pocket mode package
    Signed-off-by: liquid0624 <rjbirnesser@gmail.com>

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -1,12 +1,11 @@
-LOCAL_PATH := $(call my-dir)
-
+LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := OnePlusPocketMode
+LOCAL_PACKAGE_NAME := OneplusPocketMode
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
@@ -14,3 +13,5 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
