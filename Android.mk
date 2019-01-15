3660c100c77f5a1c47c1d26f45533ca2799299d7
Tim <tim@twlw.ca>
2019-01-15 11:38:18 -0800

    sdm845-common: Add some libs from omni

--- Android.mk
+++ Android.mk
@@ -17,6 +17,30 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter enchilada,$(TARGET_DEVICE)),)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),enchilada))
+subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+
+include $(CLEAR_VARS)
+
+IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "IMS lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
+
+NXP_LIB := libnxpnfc_nci_jni.so
+NXP_SYMLINKS := $(addprefix $(TARGET_OUT_APPS)/NxpNfcNci/lib/arm64/,$(notdir $(NXP_LIB)))
+$(NXP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "NXP lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(NXP_SYMLINKS)
 endif
